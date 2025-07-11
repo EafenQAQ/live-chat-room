2d417385070b5d7980dba0a6950a99d92ede7a39
EafenQAQ <eafenqaq@gmail.com>
2025-07-11 11:51:57 +0800

    git init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WelcomePage from '@/views/WelcomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: WelcomePage,
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue'),
+    // },
+  ],
+})
+
+export default router
