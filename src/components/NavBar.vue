3c662d6db9d80fe1597c77c9c677c62a89eca602
EafenQAQ <eafenqaq@gmail.com>
2025-07-12 12:47:12 +0800

    新增：登出键

---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav v-if="user" id="NavBar">
+    <div class="text-area">
+      <p>你好！{{ user.displayName }}</p>
+      <p>你的邮箱：{{user.email}}</p>
+    </div>
+    <button @click="handleClick">
+      注销
+    </button>
+
+  </nav>
+</template>
+
+<script setup>
+import { useLogout } from '@/composables/useLogout';
+import { getUser } from '@/composables/getUser';
+import { useRouter } from 'vue-router';
+
+const { logout, error } = useLogout()
+const { user } = getUser()
+const router = useRouter();
+
+const handleClick = async () => {
+  await logout()
+  if (!error.value) {
+    console.log('注销成功！')
+    router.push({ name: 'welcome' })
+  }
+}
+
+</script>
+
+<style scoped>
+button {
+  background-color: var(--error-color);
+}
+
+button:hover {
+  background-color: hsl(from var(--error-color) h calc(s - 20) calc(l - 20));
+}
+
+/* 导航栏基本样式 */
+#NavBar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 2rem;
+  /* 宽度 */
+  width: 100%;
+
+  border-bottom: 2px solid var(--base-color-600);
+
+  /* 下阴影 */
+  box-shadow: 0px 2rem 2rem 5px var(--base-color-100);
+}
+
+.text-area {
+  display: flex;
+
+  gap: 0.5rem;
+}
+</style>
--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-
+  <div id="chatroom">
+    <h1>Chatroom</h1>
+    <NavBar />
   </div>
 </template>
 
 <script setup>
+import NavBar from '@/components/NavBar.vue';
+
 
 </script>
 
